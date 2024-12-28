48176be8f7f339e89b17d1116ff0d59ac942d78b
LeeChangJoonJoonJoon <hooninpark@e8igh.co.kr>
2024-12-28 17:15:51 +0900

    438

--- Chapter11/AddressBook.c
+++ Chapter11/AddressBook.c
@@ -4,9 +4,18 @@
 #include <stdio.h>
 
 // 충격적인 사실 : C언어의 구조체는 public, private이 없고, 내부에 함수를 정의할 수도 없다.
+struct SNode
+{
+    char* m_sUserName;
+    char* m_sUserPhoneNum;
+
+    struct SNode* m_pNextNode;
+};
+
 struct SAddressBook
 {
 
+    struct SNode* m_HeadNode;
 };
 
 int main()
---
+++ Chapter11/USERDATA.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Changjoon Lee on 2024. 12. 28..
+//
+
+#include "USERDATA.h"
+
+CUSERDATA::CUSERDATA()
+{
+
+}
+
+CUSERDATA::~CUSERDATA()
+{
+
+}
