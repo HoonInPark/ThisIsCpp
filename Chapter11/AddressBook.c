587c2dc173f74cda2186c3160e855803cff43079
LeeChangJoonJoonJoon <hooninpark@e8igh.co.kr>
2024-12-28 16:46:29 +0900

    437

--- Chapter11/AddressBook.c
+++ Chapter11/AddressBook.c
@@ -1,3 +1,15 @@
 //
 // Created by Changjoon Lee on 2024. 12. 27..
 //
+#include <stdio.h>
+
+// 충격적인 사실 : C언어의 구조체는 public, private이 없고, 내부에 함수를 정의할 수도 없다.
+struct SAddressBook
+{
+
+};
+
+int main()
+{
+
+}
---
+++ Chapter11/AddressBook_OOP.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Changjoon Lee on 2024. 12. 28..
+//
+#include "bits/stdc++.h"
+using namespace std;
+
+int main()
+{
+
+}
